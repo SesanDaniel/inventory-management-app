61720ddbe1a246b646406a546025af9d46b41226
Daniel Oluwaseyi <104599111+SesanDaniel@users.noreply.github.com>
2026-07-19 21:48:02 +0400

    Update DesktopShell.tsx 1

--- src/components/DesktopShell.tsx
+++ src/components/DesktopShell.tsx
@@ -15,14 +15,14 @@ export default function DesktopShell({ listPane, detailPane, hasSelection }: Des
       </div>
 
       {/* Right: detail / form / active content pane */}
-      <div className="flex-1 flex flex-col overflow-hidden relative">
+      <div className="flex-1 flex flex-col overflow-hidden relative min-h-0">
         <div className="absolute inset-0 z-0 pointer-events-none overflow-hidden">
           <div className="absolute -top-[10%] -left-[10%] w-[50%] h-[45%] bg-indigo-600/10 rounded-full blur-[80px]" />
           <div className="absolute bottom-[10%] -right-[10%] w-[50%] h-[45%] bg-purple-600/10 rounded-full blur-[70px]" />
         </div>
         <div
           key={hasSelection ? 'selected' : 'empty'}
-          className="animate-fade-in flex-1 z-10 flex flex-col overflow-hidden"
+          className="animate-fade-in flex-1 z-10 flex flex-col overflow-hidden min-h-0"
         >
           {hasSelection ? (
             detailPane
